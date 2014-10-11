5097379d4eb49dacadee3ae8fb94f947b437524a
Daan Wilmer BSc <daan@daanwilmer.nl>
2014-10-11 13:22:58 +0200

    Extracted Testdata from classes

---
+++ Testdata.py
@@ -0,0 +1,39 @@
+#! /usr/bin/python
+
+class Testdata:
+	def __init__(self, pos):
+		self.pos = pos
+		self.results = {}
+
+	def addTest(self, delayfilename):
+		from solver import solve
+		f = open(delayfilename, 'r')
+		delays = map(float, list(f))
+		f.close()
+
+		delayedProblem = self.pos.clone()
+		for (index, act) in enumerate(delayedProblem.activities):
+			act.time *= delays[index]
+			
+		toSolve = {'options':{'solver':"noResources"},'instance':delayedProblem}
+		delayedSolution = solve(toSolve)
+
+		self.results[delayfilename] = delayedSolution.startTimes
+
+	def writeToFile(self, filename):
+		outfile = open(filename, 'w')
+		for (delayfile, schedule) in self.results.iteritems():
+			outfile.write(delayfile)
+			outfile.write(':')
+			outfile.write(' '.join(map(str, schedule)))
+			outfile.write('\n')
+		outfile.close()
+
+	def readFromFile(self, filename):
+		infile = open(filename, 'r')
+		for line in infile:
+			parts = line.split(':')
+			if len(parts) == 2:
+				self.results[parts[0]] = map(float, parts[1].split())
+		infile.close()
+
--- classes.py
+++ classes.py
@@ -1,46 +1,9 @@
 #! /usr/bin/python
 
-import sys, string
 import Activity
 import Instance
 import Solution
-
-class Testdata:
-	def __init__(self, pos):
-		self.pos = pos
-		self.results = {}
-
-	def addTest(self, delayfilename):
-		from solver import solve
-		f = open(delayfilename, 'r')
-		delays = map(float, list(f))
-		f.close()
-
-		delayedProblem = self.pos.clone()
-		for (index, act) in enumerate(delayedProblem.activities):
-			act.time *= delays[index]
-			
-		toSolve = {'options':{'solver':"noResources"},'instance':delayedProblem}
-		delayedSolution = solve(toSolve)
-
-		self.results[delayfilename] = delayedSolution.startTimes
-
-	def writeToFile(self, filename):
-		outfile = open(filename, 'w')
-		for (delayfile, schedule) in self.results.iteritems():
-			outfile.write(delayfile)
-			outfile.write(':')
-			outfile.write(' '.join(map(str, schedule)))
-			outfile.write('\n')
-		outfile.close()
-
-	def readFromFile(self, filename):
-		infile = open(filename, 'r')
-		for line in infile:
-			parts = line.split(':')
-			if len(parts) == 2:
-				self.results[parts[0]] = map(float, parts[1].split())
-		infile.close()
+import Testdata
 		
 class Report:
 	def __init__(self):
